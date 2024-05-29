7c35b7822ed3cfcb3a49468214f050c2a3a3adaa
zzh <1914163770@qq.com>
2024-05-29 22:35:44 +0800

    1107

--- PATAdvanced/1107.cpp
+++ PATAdvanced/1107.cpp
@@ -1,67 +1,60 @@
 #include<iostream>
-#include<vector>
+#include<cstdio>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-// isRoot用来统计一个集合下节点数量，
-// 并统计有多少集合
-vector<int> p,isRoot;
-int cmp(int a,int b){
+vector<int> parent,cntCycle;
+int find(int x){
+    if(x != parent[x]) parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+int cmp1(int a, int b){
     return a > b;
 }
 
-int find(int x){
-    if(x != p[x]) p[x] = find(p[x]);
-    return p[x];
-}
-
-int cc; // union 次数
-
 void Union(int a,int b){
-    int faA = find(a);
-    int faB = find(b);
-    if(faA != faB){
-        p[faA] = faB;
-        // cc++;
+    int fA = find(a);
+    int fB = find(b);
+    if(fA != fB){
+        parent[fA] = fB; 
     }
 }
 
 int main(){
-    int n,k,t,cnt = 0;
+    int n,k,t;
     // hash 记录课程对应的第一个id，用于union
-    int course[1001] = {0};
+    int course[1001];
+    fill(course,course+1001,0);
     scanf("%d",&n);
     // 下标1-n
-    p.resize(n + 1);
-    isRoot.resize(n + 1);
-    for(int i = 1;i <= n;i++){
-        p[i] = i;
-    }
+    parent.resize(n + 1);
+    cntCycle.resize(n + 1);
+    // 初始化
+    for(int i = 1;i <= n;i++)
+        parent[i] = i;
     for(int i = 1;i <= n;i++){
         scanf("%d:",&k);
         for(int j = 0;j < k;j++){
             scanf("%d",&t);
-            if(course[t] == 0){
+            if(course[t] == 0)
                 course[t] = i;
-            }
-            Union(i,find(course[t]));
+            else Union(i,course[t]);
         }
-    }
-    // cout << n - cc <<endl; 集群个数
-    // 找到每个节点的根节点，对应位置上数量++
-    for(int i = 1; i <= n;i++){
-        isRoot[find(i)]++;
     }
     for(int i = 1;i <= n;i++){
-        if(isRoot[i] != 0){
-            cnt++;
-        }
+        cntCycle[find(i)]++;
+    }
+    int cnt = 0;
+    for(int i = 1;i <= n;i++){
+        if(cntCycle[i] != 0) cnt++;
     }
     printf("%d\n",cnt);
-    sort(isRoot.begin(),isRoot.end(),cmp);
+    sort(cntCycle.begin(),cntCycle.end(),greater<int>());
     for(int i = 0;i < cnt;i++){
-        if(i!=0)printf(" ");
-        printf("%d",isRoot[i]);
+        if(i != 0) printf(" ");
+        printf("%d",cntCycle[i]);
     }
     return 0;
 }
