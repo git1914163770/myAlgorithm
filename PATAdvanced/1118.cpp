f57f8c48872fbb23aa1b6c64c9d1a3f172a59ece
zzh <1914163770@qq.com>
2024-05-30 13:47:34 +0800

    1118

--- PATAdvanced/1118.cpp
+++ PATAdvanced/1118.cpp
@@ -1,44 +1,61 @@
+#include<iostream>
+#include<vector>
+#include<cstdio>
 // https://blog.csdn.net/Weary_PJ/article/details/125032759
 // https://blog.csdn.net/liuchuo/article/details/52505109
-
-#include<iostream>
-#include<cstdio>
 using namespace std;
-
+int n;
 const int N = 10010;
-int n,idx,k;
-int p[N],birds[N];
-
+int p[N],cntbird[N];
+bool visit[N] = {false};
 int find(int x){
     if(p[x] != x) p[x] = find(p[x]);
     return p[x];
 }
 
+void Union(int a, int b){
+    int fA = find(a);
+    int fB = find(b);
+    if(fA!=fB) p[fA] = fB;
+}
+
 int main(){
-    scanf("%d",&n);
     for(int i = 0;i < N;i++){
         p[i] = i;
     }
-    int max_id = 1,cnt = 0;
-    while(n--){
+    scanf("%d",&n);
+    int k;
+    for(int i = 0;i < n;i++){
         scanf("%d",&k);
-        for(int i = 0;i < k;i++){
-            scanf("%d",&birds[i]);
-            if(max_id < birds[i]) max_id = birds[i];
+        int a,b;
+        if(k != 0) {
+            scanf("%d",&a);
+            visit[a] = true;
         }
-        int pa = find(birds[0]);
-        for(int i = 1;i < k;i++){
-            int pb = find(birds[i]);
-            if(pa != pb){
-                p[pb] = pa;
-                cnt++;
-            }
+        for(int j = 1;j < k;j++){
+            scanf("%d",&b);
+            visit[b] = true;
+            Union(a,b);
         }
     }
-    printf("%d %d\n",max_id - cnt,max_id);
+    int cnt = 0,sum = 0;
+    for(int i = 0;i < N;i++){
+        if(visit[i]){
+            sum++;
+            cntbird[find(i)]++;
+        }
+    }
+
+    for(int i = 0;i < N;i++){
+        if(cntbird[i] != 0){
+            cnt++;
+            // sum += cntbird[i];
+        }
+    }
+    printf("%d %d\n",cnt,sum);
+    int a,b;
     scanf("%d",&k);
     while(k--){
-        int a,b;
         scanf("%d %d",&a,&b);
         if(find(a) == find(b)) printf("Yes\n");
         else printf("No\n");
