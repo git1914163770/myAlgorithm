eb5ede805a09f0db1e26d86aa7af62561a17ad7b
zzh <1914163770@qq.com>
2024-05-30 13:02:15 +0800

    1114

--- PATAdvanced/1114.cpp
+++ PATAdvanced/1114.cpp
@@ -1,90 +1,97 @@
-#include <cstdio>
-#include <algorithm>
+#include<cstdio>
+#include<algorithm>
+#include<set>
 using namespace std;
-struct DATA {
-    int id, fid, mid, num, area;
-    int cid[10];
-}d[1005];   
-struct node {
-    // people 家庭成员数
-    int id, people;
-    // num 房产数
-    double num, area;
-    // 换成vector 就不需要flag
+
+struct DATA{
+    int id,fid,mid,setnum,area;
+    vector<int> child;
+}d[10001];
+
+int parent[10001];
+
+struct node{
+    int id,peoplenum;
+    double setnum;
+    double avgarea;
     bool flag = false;
-}ans[10000];
-int father[10000];
-
-bool visit[10000];
-int find(int x) {
-    // while(x != father[x])
-    //     x = father[x];
-    // return x;
-    if(father[x] != x) father[x] = find(father[x]);
-    return father[x];   
-}
-void Union(int a, int b) {
-    int faA = find(a);
-    int faB = find(b);
-    if(faA > faB)
-        father[faA] = faB;
-    else if(faA < faB)
-        father[faB] = faA;
-}
+}countarr[10001];
 // 平均面积降序 ID升序
-int cmp1(node a, node b) {
-    if(a.area != b.area)
-        return a.area > b.area;
-    else
-        return a.id < b.id;
+int cmp(node a,node b){
+    if(a.avgarea != b.avgarea){
+        return a.avgarea > b.avgarea;
+    }else return a.id < b.id;
 }
-int main() {
-    int n, k, cnt = 0;
-    scanf("%d", &n);
-    for(int i = 0; i < 10000; i++)
-        father[i] = i;
-    for(int i = 0; i < n; i++) {
-        scanf("%d %d %d %d", &d[i].id, &d[i].fid, &d[i].mid, &k);
-        visit[d[i].id] = true;
-        if(d[i].fid != -1) {
-            visit[d[i].fid] = true;
-            Union(d[i].fid, d[i].id);
-        }
-        if(d[i].mid != -1) {
-            visit[d[i].mid] = true;
-            Union(d[i].mid, d[i].id);
-        }
-        for(int j = 0; j < k; j++) {
-            scanf("%d", &d[i].cid[j]);
-            visit[d[i].cid[j]] = true;
-            Union(d[i].cid[j], d[i].id);
-        }
-        scanf("%d %d", &d[i].num, &d[i].area);
-    }
-    for(int i = 0; i < n; i++) {
-        int id = find(d[i].id);
-        ans[id].id = id;
-        ans[id].num += d[i].num;
-        ans[id].area += d[i].area;
-        ans[id].flag = true;
-    }
-    for(int i = 0; i < 10000; i++) {
-        // 因为是用数组存的。用vector/map存就不需要visit数组
-        if(visit[i])
-            ans[find(i)].people++;
+// 因为id 不连续，使用set/数组 保存每个出现的id
+bool visit[10000];
 
-        if(ans[i].flag)
-            cnt++;
+int find(int x){
+    if(parent[x] != x) parent[x] = find(parent[x]);
+    return parent[x];
+}
+
+void Union(int a,int b){
+    int fA = find(a);
+    int fB = find(b);
+    if(fA < fB){
+        parent[fB] = fA;
+    }else if(fB < fA){
+        parent[fA] = fB;
     }
-    for(int i = 0; i < 10000; i++) {
-        if(ans[i].flag) {
-            ans[i].num = (double)(ans[i].num * 1.0 / ans[i].people);
-            ans[i].area = (double)(ans[i].area * 1.0 / ans[i].people);
+}
+
+
+int main(){
+    int n,k;
+    scanf("%d",&n);
+    // 别忘了初始化parent
+    for(int i = 0;i < 10000;i++)
+        parent[i] = i;
+    int child;
+    for(int i = 0;i < n;i++){
+        int tempid;
+        scanf("%d",&tempid);
+        scanf("%d %d %d",&d[tempid].fid,&d[tempid].mid,&k);
+        visit[tempid] = true;
+        if(d[tempid].fid != -1){
+            Union(tempid,d[tempid].fid);
+            visit[d[tempid].fid] = true;
+        }
+        if(d[tempid].mid != -1){
+            Union(tempid,d[tempid].mid);
+            visit[d[tempid].mid] = true;
+        }
+        for(int j = 0;j < k;j++){
+            scanf("%d",&child);
+            visit[child] = true;
+            Union(tempid,child);
+        }
+        scanf("%d %d",&d[tempid].setnum,&d[tempid].area);
+    }
+    int cnt = 0;
+    // 注意这里不能是0-n ，应要对每个人计算
+    for(int i = 0;i < 10000;i++){
+        if(visit[i]){
+            int id = find(i);
+            countarr[id].id = id;
+            countarr[id].setnum += d[i].setnum;
+            countarr[id].avgarea += d[i].area;
+            countarr[id].flag = true;
+            countarr[id].peoplenum++;
         }
     }
-    sort(ans, ans + 10000, cmp1);
-    printf("%d\n", cnt);
-    for(int i = 0; i < cnt; i++)
-        printf("%04d %d %.3f %.3f\n", ans[i].id, ans[i].people, ans[i].num, ans[i].area);
+    for(int i = 0;i < 10000;i++){
+        // 注意要上面完整求出人数后才能计算平均值，所以两个for循环不能合在一起
+        if(countarr[i].flag){
+            cnt++;
+            countarr[i].setnum = countarr[i].setnum * 1.0 / countarr[i].peoplenum;
+            countarr[i].avgarea = countarr[i].avgarea * 1.0 / countarr[i].peoplenum;
+        }
+    }
+    sort(countarr,countarr + 10000,cmp);
+    printf("%d\n",cnt);
+    for(int i = 0;i < cnt;i++){
+        printf("%04d %d %.3f %.3f\n",countarr[i].id,countarr[i].peoplenum,countarr[i].setnum,countarr[i].avgarea);
+    }
     return 0;
 }
