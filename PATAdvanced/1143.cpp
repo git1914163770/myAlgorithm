1291c77b2c8a6cc647bdf5134249541e686c326a
zzh <1914163770@qq.com>
2024-05-31 10:56:20 +0800

    1143

--- PATAdvanced/1143.cpp
+++ PATAdvanced/1143.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 map<int,bool> mp;
 // https://blog.csdn.net/liuchuo/article/details/79618849
+// https://programmercarl.com/0235.%E4%BA%8C%E5%8F%89%E6%90%9C%E7%B4%A2%E6%A0%91%E7%9A%84%E6%9C%80%E8%BF%91%E5%85%AC%E5%85%B1%E7%A5%96%E5%85%88.html#%E6%80%9D%E8%B7%AF
 int main(){
     int m,n,u,v,a;
     scanf("%d %d",&m,&n);
@@ -17,6 +18,7 @@ int main(){
         for(int j = 0;j < n;j++){
             a = pre[j];
             if((a > u && a < v) || (a > v && a < u) || (a == u) || (a == v))break;
+            // if(!((a > u && a > v) || (a < u && a < v))) break;
         }
         
         if (mp[u] == false && mp[v] == false)
